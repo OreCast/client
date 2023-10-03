ff6f968282c4b2ff29b1cf2ae6e8c9d827b7632d
Valentin Kuznetsov <vkuznet@gmail.com>
2023-10-03 11:01:43 -0400

    Put first implementation

--- cmd/meta.go
+++ cmd/meta.go
@@ -1,7 +1,72 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/spf13/cobra"
+)
+
+// helper function to get metadata
+// MetaData represents MetaData object returned from discovery service
+type MetaData struct {
+	ID          string   `json:"id"`
+	Site        string   `json:"site"`
+	Description string   `json:"description"`
+	Tags        []string `json:"tags"`
+	Bucket      string   `json:"bucket"`
+}
+
+// MetaDataRecord represents MetaData record returned by discovery service
+type MetaDataRecord struct {
+	Status string     `json:"status"`
+	Data   []MetaData `json:"data"`
+}
+
+// helper function to fetch sites info from discovery service
+func metadata(site string) MetaDataRecord {
+	var results MetaDataRecord
+	rurl := fmt.Sprintf("%s/meta/%s", _oreConfig.Services.MetaDataURL, site)
+	resp, err := http.Get(rurl)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return results
+	}
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&results); err != nil {
+		log.Println("ERROR:", err)
+		return results
+	}
+	return results
+}
+
+func getMeta(site string) ([]MetaData, error) {
+	var records []MetaData
+	sites, err := getSites()
+	if err != nil {
+		return records, err
+	}
+	for _, sobj := range sites {
+		if site == sobj.Name {
+			if verbose > 0 {
+				fmt.Printf("processing %+v\n", sobj)
+			}
+			rec := metadata(site)
+			if rec.Status == "ok" {
+				for _, r := range rec.Data {
+					records = append(records, r)
+				}
+			} else {
+				fmt.Printf("WARNING: failed metadata record %+v\n", rec)
+			}
+		}
+	}
+	return records, nil
+}
 func metaCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "meta",
@@ -10,6 +75,19 @@ func metaCommand() *cobra.Command {
 	Complete documentation is available at https://orecast.com/documentation/`,
 		Run: func(cmd *cobra.Command, args []string) {
 			// Do Stuff Here
+			site := "Cornell"
+			records, err := getMeta(site)
+			if err != nil {
+				fmt.Println("ERROR", err)
+				os.Exit(1)
+			}
+			for _, r := range records {
+				fmt.Println("---")
+				fmt.Printf("ID         : %s\n", r.ID)
+				fmt.Printf("Tags       : %v\n", r.Tags)
+				fmt.Printf("Bucket     : %v\n", r.Bucket)
+				fmt.Printf("Description: %s\n", r.Description)
+			}
 		},
 	}
 	return cmd
--- cmd/site.go
+++ cmd/site.go
@@ -1,6 +1,39 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+// Site represents Site object returned from discovery service
+type Site struct {
+	Name         string `json:"name" form:"name" binding:"required"`
+	URL          string `json:"url" form:"url" binding:"required"`
+	Endpoint     string `json:"endpoint" form:"endpoint" binding:"required"`
+	AccessKey    string `json:"access_key" form:"access_key" binding:"required"`
+	AccessSecret string `json:"access_secret" form:"access_secret" binding:"required"`
+	UseSSL       bool   `json:"use_ssl" form:"use_ssl"`
+	Description  string `json:"description" form:"description"`
+}
+
+func getSites() ([]Site, error) {
+	var out []Site
+	rurl := fmt.Sprintf("%s/sites", _oreConfig.Services.DiscoveryURL)
+	resp, err := http.Get(rurl)
+	if err != nil {
+		return out, err
+	}
+	defer resp.Body.Close()
+	var results []Site
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&results); err != nil {
+		return out, err
+	}
+	return results, nil
+}
 
 func siteCommand() *cobra.Command {
 	cmd := &cobra.Command{
@@ -9,7 +42,16 @@ func siteCommand() *cobra.Command {
 		Long: `OreCast site command
                 Complete documentation is available at https://orecast.com/documentation/`,
 		Run: func(cmd *cobra.Command, args []string) {
-			// Do Stuff Here
+			if sites, err := getSites(); err == nil {
+				for _, s := range sites {
+					fmt.Println("---")
+					fmt.Printf("Name       : %s\n", s.Name)
+					fmt.Printf("URL        : %s\n", s.URL)
+					fmt.Printf("Description: %s\n", s.Description)
+				}
+			} else {
+				fmt.Println("ERROR", err)
+			}
 		},
 	}
 	return cmd
