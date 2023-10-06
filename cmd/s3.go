1ed62e0e21da01a50f938097f45392bae6453a8e
Valentin Kuznetsov <vkuznet@gmail.com>
2023-10-06 11:17:21 -0400

    Implement upload and delete actions

--- cmd/s3.go
+++ cmd/s3.go
@@ -1,10 +1,15 @@
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -15,7 +20,15 @@ type StorageRecord struct {
 	Data   any    `json:"data"`
 }
 
-// helper function to provide usage of s3 option
+// UploadRecord represents Storage record returned by datamanagement service
+type UploadRecord struct {
+	Status string `json:"status"`
+	Error  string `json:"error"`
+	Msg    string `json:"msg"`
+	Object any    `json:"object"`
+}
+
+// helper function to provide s3 usage info
 func s3Usage() {
 	fmt.Println("orecast s3 <ls|create|delete|upload> [value]")
 	fmt.Println("Examples:")
@@ -45,7 +58,7 @@ func s3List(args []string) {
 		os.Exit(1)
 	}
 	bucketName := args[1]
-	fmt.Printf("INFO: list bucket %s", bucketName)
+	fmt.Printf("INFO: list bucket %s\n", bucketName)
 
 	var results StorageRecord
 	rurl := fmt.Sprintf("%s/storage/%s", _oreConfig.Services.DataManagementURL, bucketName)
@@ -63,7 +76,7 @@ func s3List(args []string) {
 		fmt.Println("ERROR:", err)
 		os.Exit(1)
 	}
-	fmt.Printf("results: %+v", results)
+	fmt.Printf("results: %+v\n", results)
 }
 
 // helper function to create new bucket on s3 storage
@@ -78,7 +91,35 @@ func s3Create(args []string) {
 		os.Exit(1)
 	}
 	bucketName := args[1]
-	fmt.Printf("INFO: create bucket %s", bucketName)
+	fmt.Printf("INFO: create bucket %s\n", bucketName)
+	var results StorageRecord
+	rurl := fmt.Sprintf("%s/storage/%s", _oreConfig.Services.DataManagementURL, bucketName)
+	if verbose > 0 {
+		fmt.Println("HTTP POST", rurl)
+	}
+	resp, err := http.Post(rurl, "", nil)
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&results); err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+	fmt.Printf("results: %+v\n", results)
+}
+
+// isDirectory determines if a file represented
+// by `path` is a directory or not
+func isDirectory(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+
+	return fileInfo.IsDir(), err
 }
 
 // helper function to upload file or directory to bucket on s3 storage
@@ -94,7 +135,86 @@ func s3Upload(args []string) {
 	}
 	bucketName := args[1]
 	fobj := args[2]
-	fmt.Printf("INFO: upload %s to bucket %s", fobj, bucketName)
+	var files []string
+	isDir, err := isDirectory(fobj)
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+	if isDir {
+		if dirFiles, err := ioutil.ReadDir(fobj); err == nil {
+			for _, file := range dirFiles {
+				if !file.IsDir() {
+					files = append(files, file.Name())
+				}
+			}
+		}
+	} else {
+		files = append(files, fobj)
+	}
+	for _, f := range files {
+		fname := filepath.Join(fobj, f)
+		fmt.Printf("INFO: upload %s to bucket %s\n", fname, bucketName)
+		rurl := fmt.Sprintf("%s/storage/%s/%s", _oreConfig.Services.DataManagementURL, bucketName, f)
+		if verbose > 0 {
+			fmt.Println("HTTP POST", rurl)
+		}
+		// open file and read its content
+		// TODO: we may need buffer stream to reduce RAM utilization
+		file, err := os.Open(fname)
+		if err != nil {
+			fmt.Println("ERROR", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		// send POST request to DataManagement service with file data content
+		/*
+		   ```
+		    curl -X POST http://localhost:8340/storage/cornell/s3-bucket/archive.zip \
+		     -F "file=@/path/test.zip" \
+		     -H "Content-Type: multipart/form-data"
+		   ```
+		*/
+		// prepare our payload by reading the local file and passing it to
+		// multipart writer
+		var buf bytes.Buffer
+		var errBuf error
+		w := multipart.NewWriter(&buf)
+		if fw, err := w.CreateFormFile("file", file.Name()); err == nil {
+			if _, err := io.Copy(fw, file); err != nil {
+				errBuf = err
+			}
+		} else {
+			errBuf = err
+		}
+		w.Close()
+		if errBuf != nil {
+			fmt.Println("ERROR:", errBuf)
+			os.Exit(1)
+		}
+
+		req, err := http.NewRequest("POST", rurl, &buf)
+		if err != nil {
+			fmt.Println("ERROR:", err)
+			os.Exit(1)
+		}
+		req.Header.Set("Content-Type", w.FormDataContentType())
+		client := http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("ERROR:", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		dec := json.NewDecoder(resp.Body)
+		var results UploadRecord
+		if err := dec.Decode(&results); err != nil {
+			fmt.Println("ERROR:", err)
+			os.Exit(1)
+		}
+		fmt.Printf("results: %+v\n", results)
+	}
 }
 
 // helper function to delete bucket on s3 storage
@@ -109,7 +229,34 @@ func s3Delete(args []string) {
 		os.Exit(1)
 	}
 	bucketName := args[1]
-	fmt.Printf("INFO: delete bucket %s", bucketName)
+	fmt.Printf("INFO: delete bucket %s\n", bucketName)
+	var results StorageRecord
+	rurl := fmt.Sprintf("%s/storage/%s", _oreConfig.Services.DataManagementURL, bucketName)
+	if verbose > 0 {
+		fmt.Println("HTTP DELETE", rurl)
+	}
+	req, err := http.NewRequest("DELETE", rurl, nil)
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&results); err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+	fmt.Printf("results: %+v\n", results)
 }
 
 func s3Command() *cobra.Command {
@@ -131,7 +278,7 @@ func s3Command() *cobra.Command {
 			} else if args[0] == "upload" {
 				s3Upload(args)
 			} else {
-				fmt.Printf("WARNING: unsupported option(s) %+v", args)
+				fmt.Printf("WARNING: unsupported option(s) %+v\n", args)
 			}
 		},
 	}
